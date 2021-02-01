096fb3a64ee80a82a4b18b9006c050bd29db36a7
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-01 17:07:10 +0100

    add sim timestep variables

--- Population/simulation.h
+++ Population/simulation.h
@@ -13,10 +13,17 @@ class Simulation
 {
 public:
     Simulation() = default;
+    Simulation(double travel_probability) : travel_probability_(travel_probability) {};
     void addPopulation(Configuration config){
-        populations_.emplace_back(config,++size_populations_);
+        end_nbr_timesteps_ = config.nbr_timesteps;
+        populations_.emplace_back(config, ++size_populations_);
     }
     void start(){
+        // while(nbr_timesteps_ < end_nbr_timesteps_){
+        //     for(auto& population: populations_){
+        //     population.nextTimestep();
+        // } 
+        // }
         for(auto& population: populations_){
             population.startSimulation();
         }
@@ -25,5 +32,8 @@ public:
 private:
     std::vector<Population> populations_;
     size_t size_populations_ = 0;
+    double travel_probability_ = .5;
     double time_;
+    size_t nbr_timesteps_ = 0;
+    size_t end_nbr_timesteps_ = 0;
 };
