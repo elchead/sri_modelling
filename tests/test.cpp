092e209551b12e0615f44b0e26c471b061878162
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-04 17:06:09 +0100

    comment about vckpg

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch_amalgamated.hpp"
-//#include <catch2/catch.hpp>
+//#include <catch2/catch.hpp> // when using VCPKG, include this header instead
 #include "simulation.h"
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
