ff4d25a19d21465a5f8736f37502baa3e699449f
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-04 17:03:43 +0100

    add catch library for testing

---
+++ tests/test.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch_amalgamated.hpp"
+//#include <catch2/catch.hpp>
+#include "simulation.h"
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    auto sim = Simulation();
+    auto config = readConfig();
+    size_t nbr_populations = 1;
+    for (int i = 0; i < nbr_populations; i++){
+        sim.addPopulation(config);
+    }
+    sim.start();
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
