059d0e9feac06ffa351443f3238be83efa3efc2e
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-04 09:44:19 +0100

    improve perf of random number function

--- Population/random.h
+++ Population/random.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <random>
-
+#include <cstdlib>
 class Random
 {
 public:
     double get_double(double min=0,double max=1) const{
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> unif_gen(min, max);
-    return unif_gen(gen);
+    // std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    // std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    // std::uniform_real_distribution<> unif_gen(min, max);
+    // return unif_gen(gen);
+    return (double) rand() / (RAND_MAX);
 }
 };
