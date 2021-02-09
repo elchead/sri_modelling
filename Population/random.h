5d4a611af7072b72bccc07622497f9065ffb76e3
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-02-09 09:51:00 +0100

    fix random min max bug

--- Population/random.h
+++ Population/random.h
@@ -5,10 +5,6 @@ class Random
 {
 public:
     double get_double(double min=0,double max=1) const{
-    // std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    // std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    // std::uniform_real_distribution<> unif_gen(min, max);
-    // return unif_gen(gen);
-    return (double) rand() / (RAND_MAX);
+    return min+ (max-min)*(double)rand() / (RAND_MAX);
 }
 };
